044054408a9d4997e49bb57d7744e3bc76813267
Theuno <theuno@gmail.com>
2014-08-31 20:38:32 +0000

    Create class for max7456

--- max7456.py
+++ max7456.py
@@ -7,71 +7,78 @@ import spidev
 import time
 from ctypes import *
 
-# Open a SPI port - max7456 connected on SPI0
-spi = spidev.SpiDev() 
-spi.open(0, 0) 
-spi.max_speed_hz = 500000
-spi.bits_per_word = 8
-spi.cshigh = False
-spi.lsbfirst = False
-spi.mode = 0
+class max7456():
+    # Create a SPI
+    spi = spidev.SpiDev() 
 
-# MAX7456 opcodes
-DMM_reg  = 0x04
-DMAH_reg = 0x05
-DMAL_reg = 0x06
-DMDI_reg = 0x07
-VM0_reg  = 0x00
-VM1_reg  = 0x01
+    # MAX7456 opcodes
+    DMM_reg  = 0x04
+    DMAH = 0x05
+    DMAL = 0x06
+    DMDI = 0x07
+    VM0_reg  = 0x00
+    VM1_reg  = 0x01
 
-# PAL - VM0_reg commands
-ENABLE_display      = 0x48
-ENABLE_display_vert = 0x4c
-MAX7456_reset       = 0x42
-DISABLE_display     = 0x40
+    # PAL - VM0_reg commands
+    ENABLE_display      = 0x48
+    ENABLE_display_vert = 0x4c
+    MAX7456_reset       = 0x42
+    DISABLE_display     = 0x40
 
-MAX_screen_rows = 13
+    MAX_screen_rows = 13
 
-# White levels
-WHITE_level_80  = 0x03
-WHITE_level_90  = 0x02
-WHITE_level_100 = 0x01
-WHITE_level_120 = 0x00
+    # White levels
+    WHITE_level_80  = 0x03
+    WHITE_level_90  = 0x02
+    WHITE_level_100 = 0x01
+    WHITE_level_120 = 0x00
 
+    def __init__(self):
+        # Open a SPI port - max7456 connected on SPI0
+        self.spi.open(0, 0)
+        self.spi.max_speed_hz = 500000
+        self.spi.bits_per_word = 8
+        self.spi.cshigh = False
+        self.spi.lsbfirst = False
+        self.spi.mode = 0
+
+        # Set all rows at the same white level
+        for x in range (0, self.MAX_screen_rows):
+          self.spi.xfer2([(x + 0x10), self.WHITE_level_90])
+
+        # Enable max7456
+        self.spi.xfer2([self.VM0_reg, self.ENABLE_display]);
+
+    def testText(self):
+        # Program test text
+        x = 25
+	
+        self.spi.xfer2([self.DMAH,0x01]) #DMAH
+
+        self.spi.xfer2([self.DMAL,x]) # DMAL
+        self.spi.xfer2([self.DMDI,0x1D])
+	
+        self.spi.xfer2([self.DMAL,x+1]) # DMAL
+        self.spi.xfer2([self.DMDI,0x0B])
+	
+        self.spi.xfer2([self.DMAL,x+2]) # DMAL
+        self.spi.xfer2([self.DMDI,0x17])
+	
+        self.spi.xfer2([self.DMAL,x+3])
+        self.spi.xfer2([self.DMDI,0x1A])
+	
+        self.spi.xfer2([self.DMAL,x+4])
+        self.spi.xfer2([self.DMDI,0x16])
+	
+        self.spi.xfer2([self.DMAL,x+5])
+        self.spi.xfer2([self.DMDI,0x0F])
+	
+        # Enable display
+        self.spi.xfer2([self.VM0_reg, self.ENABLE_display]);
+    
 try:
-  # Set all rows at the same white level
-  for x in range (0, MAX_screen_rows):
-    print "Row: ", x
-    spi.xfer2([(x + 0x10), WHITE_level_90])
-
-  # Enable max7456
-  spi.xfer2([VM0_reg, ENABLE_display]);
-
-  # Program test text
-  x = 25
-	
-  spi.xfer2([0x05,0x01]) #DMAH
-
-  spi.xfer2([0x06,x]) # DMAL
-  spi.xfer2([0x07,0x1D])
-	
-  spi.xfer2([0x06,x+1]) # DMAL
-  spi.xfer2([0x07,0x0B])
-	
-  spi.xfer2([0x06,x+2]) # DMAL
-  spi.xfer2([0x07,0x17])
-	
-  spi.xfer2([0x06,x+3])
-  spi.xfer2([0x07,0x1A])
-	
-  spi.xfer2([0x06,x+4])
-  spi.xfer2([0x07,0x16])
-	
-  spi.xfer2([0x06,x+6])
-  spi.xfer2([0x07,0x0F])
-	
-  # Enable display
-  spi.xfer2([VM0_reg, ENABLE_display]);
+    max7456 = max7456()
+    max7456.testText()
     
 except KeyboardInterrupt:
     spi.close() 
