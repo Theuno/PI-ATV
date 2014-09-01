fe54ecee7ce86e7d2b563119934d13390e4b606e
Theuno <theuno@gmail.com>
2014-09-01 21:37:58 +0000

    Add text funtion, and char map.

--- max7456.py
+++ max7456.py
@@ -33,6 +33,16 @@ class max7456():
     WHITE_level_100 = 0x01
     WHITE_level_120 = 0x00
 
+    chars = {' ':0, '1':1, '2':2, '3':3, '4':4, '5':5, '6':6, '7':7, '8':8, '9':9,
+        '0':10, 'A':11, 'B':12, 'C':13, 'D':14, 'E':15, 'F':16, 'G':17, 'H':18, 'I':19,
+        'J':20, 'K':21, 'L':22, 'M':23, 'N':24, 'O':25, 'P':26, 'Q':27, 'R':28, 'S':29,
+        'T':30, 'U':31, 'V':32, 'W':33, 'X':34, 'Y':35, 'Z':36, 'a':37, 'b':38, 'c':39,
+        'd':40, 'e':41, 'f':42, 'g':43, 'h':44, 'i':45, 'j':46, 'k':47, 'l':48, 'm':49,
+        'n':50, 'o':51, 'p':52, 'q':53, 'r':54, 's':55, 't':56, 'u':57, 'v':58, 'x':59,
+        'y':60, 'z':61, '(':62, ')':63, '.':64, '?':65, ';':66, ':':67, ',':68, '\'':69,
+        '/':70, '"':71, '-':72, '&lt;':73, '&gt;':74, '@':75, '\xa9':76
+    }
+
     def __init__(self):
         # Open a SPI port - max7456 connected on SPI0
         self.spi.open(0, 0)
@@ -41,6 +51,10 @@ class max7456():
         self.spi.cshigh = False
         self.spi.lsbfirst = False
         self.spi.mode = 0
+  
+        # On init, reset max7456
+        self.spi.xfer2([self.VM0_reg, self.MAX7456_reset])
+        time.sleep(0.2)
 
         # Set all rows at the same white level
         for x in range (0, self.MAX_screen_rows):
@@ -48,6 +62,25 @@ class max7456():
 
         # Enable max7456
         self.spi.xfer2([self.VM0_reg, self.ENABLE_display]);
+
+    def printStr(self, X, Y, str, enable = True):
+        disp = []
+        for char in str:
+            if self.chars.has_key(char):
+                disp.append(self.chars[char])
+            else:
+                disp.append(0x00)
+        disp.append(0xFF)
+
+        start = Y * 30 + X
+        self.spi.xfer2([self.VM0_reg, self.DISABLE_display])
+        self.spi.xfer2([self.DMAL, start])
+        self.spi.xfer2([self.DMM_reg, 0x01]) # TODO: Define
+        print disp
+        for char in disp:
+            self.spi.xfer2([(char)])
+        self.spi.xfer2([self.VM0_reg, self.ENABLE_display_vert])
+
 
     def testText(self):
         # Program test text
@@ -74,11 +107,13 @@ class max7456():
         self.spi.xfer2([self.DMDI,0x0F])
 	
         # Enable display
-        self.spi.xfer2([self.VM0_reg, self.ENABLE_display]);
+        self.spi.xfer2([self.VM0_reg, self.ENABLE_display_vert]);
+
     
 try:
     max7456 = max7456()
-    max7456.testText()
+    #max7456.testText()
+    max7456.printStr(2, 3, "Hello PA5PT")
     
 except KeyboardInterrupt:
     spi.close() 
